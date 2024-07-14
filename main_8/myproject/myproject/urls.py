bb36e223c6ce058ce2747ae1a91dc576ebb53e33
Marwan Wafik <Marwanewafik@hotmail.fr>
2024-07-14 18:44:29 +0200

    Main

---
+++ main_8/myproject/myproject/urls.py
@@ -0,0 +1,11 @@
+# myproject/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+    path('', views.index, name='base'),
+    path('accounts/', include('django.contrib.auth.urls')),  # Include built-in auth URLs
+]
